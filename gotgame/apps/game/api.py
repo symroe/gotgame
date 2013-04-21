6ba6222cd657b09eab51077bfd9a805729f08cad
Marco Fucci <marcofucci@gmail.com>
2013-04-21 10:43:52 +0100

    Implemented bank endpoint

--- gotgame/apps/game/api.py
+++ gotgame/apps/game/api.py
@@ -4,7 +4,7 @@ from tastypie.resources import ModelResource
 from tastypie import http
 from tastypie.utils import trailing_slash
 
-from .models import Profile
+from .models import Profile, Banked
 
 
 class ProfileResource(ModelResource):
@@ -16,10 +16,15 @@ class ProfileResource(ModelResource):
     def prepend_urls(self):
         return [
             url(r"^(?P<resource_name>%s)/add-credits%s$" % (self._meta.resource_name, trailing_slash()),
-                self.wrap_view('add_credits'), name="add_credits")
+                self.wrap_view('add_credits'), name="add_credits"),
+            url(r"^(?P<resource_name>%s)/bank%s$" % (self._meta.resource_name, trailing_slash()),
+                self.wrap_view('bank'), name="bank")
         ]
 
     def add_credits(self, request, **kwargs):
+        """
+            curl -v -X POST -H "Content-Type: application/json" --data '{"username": "myusername", "email": "player1@example.com", "credits": 5}' http://localhost:8000/api/v1/profile/add-credits/
+        """
         self.method_check(request, allowed=['post'])
 
         deserialized = self.deserialize(request, request.body, format=request.META.get('CONTENT_TYPE', 'application/json'))
@@ -46,5 +51,43 @@ class ProfileResource(ModelResource):
 
         profile.credits = params['credits']
         profile.save()
+
+        return http.HttpNoContent()
+
+
+    def bank(self, request, **kwargs):
+        """
+            curl -v -X POST -H "Content-Type: application/json" --data '{"username": "myusernamesss"}' http://localhost:8000/api/v1/profile/bank/
+
+            Banks the credits of `username` only if credits > 0.
+            Returns 404 if username not found, 204 otherwise.
+        """
+        self.method_check(request, allowed=['post'])
+
+        deserialized = self.deserialize(request, request.body, format=request.META.get('CONTENT_TYPE', 'application/json'))
+        deserialized = self.alter_deserialized_detail_data(request, deserialized)
+
+        # params
+        username = deserialized.get('username')
+
+        if not username:
+            return self.error_response(request, 'username required', response_class=http.HttpBadRequest)
+
+        # get profile
+        try:
+            profile = Profile.objects.get(username=username)
+        except Profile.DoesNotExist:
+            return http.HttpNotFound()
+
+        # bank it
+        if profile.credits:
+            Banked.objects.create(
+                    profile=profile,
+                    credits=profile.credits
+                )
+
+            # resetting profile.credits
+            profile.credits = 0
+            profile.save()
 
         return http.HttpNoContent()
