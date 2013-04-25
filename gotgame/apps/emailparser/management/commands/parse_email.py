f73b7b7674129a580df4594cca1be8c720084cb3
Sym Roe <sym.roe@talusdesign.co.uk>
2013-04-25 20:48:24 +0100

    test email pusher

--- gotgame/apps/emailparser/management/commands/parse_email.py
+++ gotgame/apps/emailparser/management/commands/parse_email.py
@@ -10,8 +10,35 @@ from emailparser.helpers import FakeEmailParser
 class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
-        # msg = email.message_from_string(sys.stdin.read())
         
-        parser = FakeEmailParser(sys.stdin.read())
-        parser.parse_message()
+        import sys
+
+        import pusher
+
+        pusher.app_id = '42602'
+        pusher.key = '03a4b4dc53485d77490b'
+        pusher.secret = '4f0d6a61c3c105ae96d0'
+
+        p = pusher.Pusher()
+
+        p['game'].trigger('end', 
+            {
+                'winner': '1',
+                '1': {
+                    'winner': True,
+                    'credits': 2,
+                    'score': 3
+                },
+                '2': {
+                    'winner': False,
+                    'credits': 0,
+                    'score': 0
+                }
+            })
         
+        
+        # # msg = email.message_from_string(sys.stdin.read())
+        # 
+        # parser = FakeEmailParser(sys.stdin.read())
+        # parser.parse_message()
+        # 
