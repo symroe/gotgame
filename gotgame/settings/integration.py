a14494f3db96bf3900a28c5d96a127ffb8c78c65
Marco Fucci <marcofucci@gmail.com>
2013-08-19 12:16:44 +0100

    added integration settings

---
+++ gotgame/settings/integration.py
@@ -0,0 +1,51 @@
+from .common import *
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+ADMINS = (
+    ('Marco Fucci', 'marcofucci@gmail.com'),
+    ('Sym Roe', 'sym.roe@talusdesign.co.uk')
+)
+
+MANAGERS = ADMINS
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'gotgame',
+        'USER': 'gotgameuser',
+        'PASSWORD': get_env_value('POSTGRES_PASSWORD'),
+        'HOST': '',
+        'PORT': '',
+    }
+}
+
+HOST_NAME = "http://integration.gotgameapp.com"
+
+# LOGGING
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'root': {
+        'level': 'WARNING',
+        'handlers': ['sentry'],
+    },
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
+        },
+    },
+    'handlers': {
+        'sentry': {
+            'level': 'WARNING',
+            'class': 'raven.contrib.django.raven_compat.handlers.SentryHandler',
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        }
+    }
+}
